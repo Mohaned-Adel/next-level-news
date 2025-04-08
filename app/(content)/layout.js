8d21bf28afe7f2558bf8e78e85ccf9116bb1ace0
Mohaned-Adel <mohaned.adel1996@gmail.com>
2025-04-08 14:03:54 +0200

    Building APIs with Route handlers

--- app/(content)/layout.js
+++ app/(content)/layout.js
@@ -1,5 +1,5 @@
-import "./globals.css";
-import MainHeader from "../components/MainHeader/MainHeader";
+import MainHeader from "@/components/MainHeader/MainHeader";
+import "../globals.css";
 
 export const metadata = {
   title: "Next.js Page Routing & Rendering",
